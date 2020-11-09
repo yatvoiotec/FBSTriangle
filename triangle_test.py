22731cff079aea466b6747adf0e7ddd1401c3f8b
yatvoiotec <sasha.barkov@gmail.com>
2020-11-09 16:50:04 +0300

    обновил README.md

--- triangle_test.py
+++ triangle_test.py
@@ -5,9 +5,10 @@ import pytest
 @pytest.mark.parametrize("point_a, point_b, point_c",
                          [
                              pytest.param([1, 1], [1, 4], [5, 1], id='positive coordinates'),
-                             pytest.param([-1, -1], [-1, -4], [-5, -1], id='negative coordinates')
+                             pytest.param([-1, -1], [-1, -4], [-5, -1], id='negative coordinates'),
+                             pytest.param([0, 0], [0, 3], [4, 0], id='zero coordinates'),
                          ])
-def test_triangle_existing_valid_points(point_a, point_b, point_c):
+def test_triangle_exists(point_a, point_b, point_c):
     triangle = Triangle(point_a, point_b, point_c)
     assert triangle.existence() is True
 
@@ -20,6 +21,6 @@ def test_triangle_existing_valid_points(point_a, point_b, point_c):
                              pytest.param([1, 1], [0, 0], [1, 1], id='a=c'),
                              pytest.param([1, 1], [2, 2], [3, 3], id='a b c on the same line')
                          ])
-def test_triangle_existing_invalid_points(point_a, point_b, point_c):
+def test_triangle_not_exists(point_a, point_b, point_c):
     triangle = Triangle(point_a, point_b, point_c)
     assert triangle.existence() is False
